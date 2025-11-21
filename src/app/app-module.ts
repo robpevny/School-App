84cc59adb13b7b17123607f2a7b24ec05257db6f
Rob_Pevny <your_email>
2025-11-20 18:46:58 -0700

    Initial commit

---
+++ src/app/app-module.ts
@@ -0,0 +1,24 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Custom } from './components/custom/custom';
+
+// you can thing of angular module like a folder in your project that tells angular what' s inside it
+@NgModule({
+  // components/directives/pipes belong to this module
+  declarations: [
+    App,
+    Custom
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
---
+++ src/app/components/custom/custom.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'custom',
+  standalone: false,
+  templateUrl: './custom.html',
+  styleUrl: './custom.css',
+})
+export class Custom {
+
+}
